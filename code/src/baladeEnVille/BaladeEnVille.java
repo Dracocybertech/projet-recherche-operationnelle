c6b6ba1788eda6bcf9e51a76f74de5dd10251a7a
netien <niels.etien@insa-rouen.fr>
2022-11-06 16:15:37 +0100

    Enfin ça fonctionne

--- code/src/baladeEnVille/BaladeEnVille.java
+++ code/src/baladeEnVille/BaladeEnVille.java
@@ -5,12 +5,19 @@ import java.io.*;
 
 public class BaladeEnVille{
 	public static void main(String[] args) {
-		int i=1;
+		// création du graphe
 		GrapheListe g = GrapheListe.deFichier("./data/reseau.txt");
 		Collection<Sommet> s = g.sommets();
+		
+		// Stockage des adresses avec le nb d'adresses posé plus haut.
 		Iterator<Sommet> iterator = s.iterator(); 
-		while (iterator.hasNext()){  
-            		System.out.println(i++ + "." + iterator.next());  
+		
+		Collection<Sommet> adresses = new LinkedList<Sommet>();
+		int i=1;
+		int nbAdresses=5;
+		while (iterator.hasNext() && (i<=nbAdresses)){
+			adresses.add(iterator.next());
+			i++;
         	}
 	}
 }
